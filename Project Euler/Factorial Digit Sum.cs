be337ff45e9210f427def66844729746d006b624
Dilan Kubay <dilankubay1@gmail.com>
2024-05-29 12:22:24 +0300

    a new folder for project euler problems has been created

---
+++ Project Euler/Factorial Digit Sum.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises.Project_Euler
+{
+    internal class Factorial_Digit_Sum
+    {
+    }
+}
