1a57919101f5865d7edfb8a34090d3feef35bd4a
Dilan Kubay <dilankubay1@gmail.com>
2024-09-12 17:38:39 +0300

    routine commit

--- Program.cs
+++ Program.cs
@@ -3,6 +3,9 @@ internal class Program
 {
      private static void Main(string[] args)
      {
+        //Digit_Factorials obj = new Digit_Factorials();
+        //Console.Write(obj.DigitFact());
         
+        Console.WriteLine("10<9");
      }
 }
