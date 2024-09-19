01ff3c19cb5a75f3c177a8a300fd3b0b85964669
Dilan Kubay <dilankubay1@gmail.com>
2024-09-19 14:41:09 +0300

    new classes

---
+++ Project Euler/Triangular, Pentagonal, and Hexagonal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises.Project_Euler
+{
+    internal class Triangular__Pentagonal__and_Hexagonal
+    {
+        public void TriangularPentagonalandHexagonal(int n)
+        {
+            int x = Triangular(n);
+            int y = Pentagonal(165);
+            int z = Hexagonal(143);
+        }
+        public int Triangular(int n)
+        {
+            return n * (n + 1) / 2;
+        }
+        public int Pentagonal(int n)
+        {
+            return n * (3*n - 1) / 2;
+        }
+        public int Hexagonal(int n)
+        {
+            return n * (2 * n - 1);
+        }
+    }
+}
