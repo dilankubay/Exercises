e78d90c267c9e91c740f603cdfcbcfcaea5fcc84
Dilan Kubay <dilankubay1@gmail.com>
2025-02-10 14:28:24 +0300

    correction

--- Project Euler/Smallest Multiple.cs
+++ Project Euler/Smallest Multiple.cs
@@ -1,3 +1,11 @@
+/*
+ 2520 is the smallest number that can be divided by each of the numbers from 1 to 20. 
+ without any remainder.
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+*/
+
+
+
 using System;
 
 class Program
